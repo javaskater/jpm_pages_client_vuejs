997938ffe2298cf77fdb86fbffa0f0f38afa0578
Jean-Pierre MENA <jpm@jpmena.eu>
2025-11-29 17:46:43 +0100

    Problem with certificates

---
+++ src/views/DiplomsView.vue
@@ -0,0 +1,25 @@
+<template>
+  <main>
+    <h2>Les diplômes</h2>
+  </main>
+</template>
+
+<script setup>
+import { reactive, ref } from 'vue'
+import { useRoute } from 'vue-router'
+
+const diploms = reactive({})
+const route = useRoute()
+const language = route.params.lang
+const fetchUser = (lang) => {
+  const url = `http://localhost/diploms/${lang}`
+  fetch(url, {})
+    .then((response) => response.json())
+    .then((result) => {
+      Object.assign(diploms, result.data)
+      diploms.data.map((x) => console.log(`${x.schoolName} - ${x.cursusDescription}`))
+    })
+}
+
+fetchUser(language)
+</script>
