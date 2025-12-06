93b3a16fbd15c04f6814b81e13406e9239af3b5b
Jean-Pierre MENA <jpm@jpmena.eu>
2025-12-06 17:03:07 +0100

    correctly logging the answers

--- src/views/DiplomsView.vue
+++ src/views/DiplomsView.vue
@@ -12,12 +12,23 @@ const diploms = reactive({})
 const route = useRoute()
 const language = route.params.lang
 const fetchUser = (lang) => {
-  const url = `http://localhost/diploms/${lang}`
-  fetch(url, {})
-    .then((response) => response.json())
+  const url = `http://localhost:8081/diploms/${lang}`
+  fetch(url, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+    },
+  })
+    .then((response) => {
+      console.log(`[then1] Retour d'une réponse pour ${url}`)
+      var jsonData = response.json()
+      console.log(`[then1] response vaut ${jsonData}`)
+      return jsonData
+    })
     .then((result) => {
-      Object.assign(diploms, result.data)
-      diploms.data.map((x) => console.log(`${x.schoolName} - ${x.cursusDescription}`))
+      console.log(`[then2] données result valent ${result}`)
+      Object.assign(diploms, result)
+      result.forEach((x) => console.log(`${x.schoolName} - ${x.cursusDescription}`))
     })
 }
 
